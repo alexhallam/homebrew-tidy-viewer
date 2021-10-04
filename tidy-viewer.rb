384a3ec0a1b356905ac94d6b63d3738b77e86e27
Alex Hallam <alex.hallam@cfacorp.com>
2021-10-04 08:20:28 -0400

    Update tidy-viewer.rb

--- tidy-viewer.rb
+++ tidy-viewer.rb
@@ -1,4 +1,4 @@
-class Tidy-Viewer < Formula
+class TidyViewer < Formula
   desc "Tidy Viewer (tv) is a cross-platform csv pretty printer that uses column styling to maximize viewer enjoyment."
   homepage "https://github.com/alexhallam/tv"
   url "https://github.com/alexhallam/tv/releases/download/0.0.20/tidy-viewer--x86_64-apple-darwin.tar.gz"
